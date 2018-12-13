89ffc29dbaa3448c43a187d631b8f2a8de0083e7
Aaron Ching <aaron.ching@locationlabs.com>
2018-12-13 13:35:52 -0800

    Add page for the 100+ verses memorization challenge

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'challenges', to: 'static_pages#challenges'
   get 'completed', to: 'static_pages#completed'
   get 'stats', to: 'static_pages#stats'
+  get 'verses', to: 'static_pages#verses'
   resources :comments, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
