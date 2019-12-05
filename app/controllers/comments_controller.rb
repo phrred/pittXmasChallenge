100b996c2f7f39734d03c62410cdf266f9ad2073
Fred Hong <fredh1993@gmail.com>
2019-12-05 16:24:16 -0500

    added edit comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,17 @@ class CommentsController < ApplicationController
 
   end
 
+  def update
+    puts "HERE"
+    @comment = Comment.find(params[:comment_id])
+    if @comment.update(comment_params)
+      flash[:success] = "Comment updated!"
+    else
+      flash[:error] = @comment.errors.full_messages.to_sentence
+    end
+    redirect_to profile_path
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:mission_id, :content, :avatar, :video)
